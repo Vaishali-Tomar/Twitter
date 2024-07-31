4b6af81f6856d8933082de89153dfb0b64e96372
Vaishali Tomar <vaishalitomar830@gmail.com>
2024-08-01 00:54:03 +0530

    new file aded

--- TwiterApp/backend/index.js
+++ TwiterApp/backend/index.js
@@ -1,19 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoute from './routes/auth.js';
+import userRoute from "./routes/user.js";
 import cookieParser from "cookie-parser";
 import connectMongoDB from './db/connectMongoDB.js';
+import postRoute from "./routes/post.js"; // Corrected import statement
+import {v2 as cloudinary} from "cloudinary";
 
 const app = express();
 
 const PORT = process.env.PORT || 7000;
 dotenv.config();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/post", postRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is up and running at port ${PORT}`);
